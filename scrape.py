ebfcefd4b1ddd2892b4eedeb560680f8930bf404
extrasauceontop <msuchemba@gmail.com>
2023-03-12 17:55:16 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,9 +1,9 @@
-from sgselenium import SgFirefox, SgChromeForCloudFlare
+from sgselenium import SgFirefox, SgChromeWithoutSeleniumWire
 from proxyfier import ProxyProviders
 
 if __name__ == "__main__":
     url = "https://7leavescafe.com/locations"
-    with SgChromeForCloudFlare(is_headless=False, proxy_provider_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER) as driver:
+    with SgChromeWithoutSeleniumWire(is_headless=False, proxy_provider_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER) as driver:
         driver.get(url)
         response = driver.page_source
 
