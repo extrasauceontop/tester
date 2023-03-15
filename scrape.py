748ad650a87f1c3994061f9b3b2e3a897340ce00
extrasauceontop <msuchemba@gmail.com>
2023-03-15 15:31:48 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,119 +1,100 @@
-from bs4 import BeautifulSoup as bs
-from sgrequests import SgRequests
-from sglogging import sglog
+import json
+from lxml import etree
+from sgselenium.sgselenium import SgChrome
 from sgscrape.sgrecord import SgRecord
-from sgscrape.sgwriter import SgWriter
 from sgscrape.sgrecord_deduper import SgRecordDeduper
-from sgscrape.sgrecord_id import RecommendedRecordIds
-from sgpostal.sgpostal import parse_address_usa
-import re
+from sgscrape.sgrecord_id import SgRecordID
+from sgscrape.sgwriter import SgWriter
+from sgzip.dynamic import DynamicZipSearch, SearchableCountries
+from sglogging import SgLogSetup
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from proxyfier import ProxyProviders
 
-DOMAIN = "maceys.com"
-BASE_URL = f"https://{DOMAIN}"
-HEADERS = {
-    "Accept": "application/json, text/plain, */*",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
-}
-MISSING = SgRecord.MISSING
-log = sglog.SgLogSetup().get_logger(logger_name=DOMAIN)
-
-
-def getAddress(raw_address: str):
+def check_response(dresponse): # noqa
     try:
-        if raw_address is not None:
-            data = parse_address_usa(raw_address)
-            street_address = ", ".join(
-                filter(lambda x: x, [data.street_address_1, data.street_address_2])
-            )
-            city = data.city or MISSING
-            state = data.state or MISSING
-            zip_postal = data.postcode or MISSING
-            return street_address, city, state, zip_postal
-    except Exception as e:
-        log.info(f"No valid address {e}")
+        WebDriverWait(driver, 1).until(EC.alert_is_present(),
+                                    'Timed out waiting for PA creation ' +
+                                    'confirmation popup to appear.')
+
+        alert = driver.switch_to.alert
+        alert.accept()
+
+    except TimeoutException:
         pass
-    return MISSING, MISSING, MISSING, MISSING
+    if "Access Denied" in driver.page_source:
+        return False
+    
+    # try:
+    #     dom = etree.HTML(driver.page_source)
+    #     json.loads(dom.xpath("//pre/text()")[0])
+    #     return True
+    
+    # except Exception:
+    #     return False
 
-
-def pull_content(http: SgRequests, url: str):
-    log.info("Pull content => " + url)
-    req = http.get(url, headers=HEADERS)
-    if req.status_code == 200:
-        return bs(req.content, "lxml")
-    return False
+    return True
 
 
 def fetch_data():
-    log.info("Fetching store_locator data")
-    with SgRequests() as http:
-        soup = pull_content(http, f"{BASE_URL}/pharmacy/locations")
-        urls = [
-            BASE_URL + url["href"]
-            for url in soup.select("div[role='list'] a[href*='/locations']")
-        ]
-        urls.append(f"{BASE_URL}/provo")
-        for page_url in urls:
-            store = pull_content(http, page_url)
-            location_name = store.find(class_="text-h2").text.strip()
-            el_addr = store.find("h3", text="Address")
-            if not el_addr:
-                raw_address = (
-                    store.find("h5", text=re.compile(r"Address.*"))
-                    .text.split(":")[-1]
-                    .strip()
-                )
-                phone = (
-                    store.find("h5", text=re.compile(r"Phone.*"))
-                    .text.split(":")[-1]
-                    .strip()
-                )
-                hours_of_operation = (
-                    store.find("h5", text=re.compile(r"Store Hours.*"))
-                    .text.split(":")[-1]
-                    .replace(";", ",")
-                    .replace("Open", "")
-                    .strip()
-                    .rstrip(",")
-                )
-            else:
-                raw_address = " ".join(
-                    el_addr.find_next("p").get_text(strip=True, separator=",").split()
-                )
-                phone = store.select_one("a[href*='tel:']").text.strip()
-                hours_of_operation = (
-                    store.find("h3", text=re.compile(r"Pharmacy Hours", flags=re.I))
-                    .find_next("div")
-                    .get_text(strip=True, separator=" ")
-                    .strip()
-                )
-            street_address, city, state, zip_postal = getAddress(raw_address)
-            country_code = "US"
-            yield SgRecord(
-                locator_domain=DOMAIN,
-                page_url=page_url,
-                location_name=location_name,
+    start_url = "https://www.soriana.com/buscador-de-tiendas"
+    post_url = "https://www.soriana.com/buscador-de-tiendas?update=true&isModal=false&radius=5.0&extendRadiusWhenSearchEmpty=true&lat=undefined&long=undefined&postalCode={}"
+    domain = "soriana.com"
+    log = SgLogSetup().get_logger(domain)
+
+    all_codes = DynamicZipSearch(
+        country_codes=[SearchableCountries.MEXICO], expected_search_radius_miles=3
+    )
+    
+    driver.get("https://www.soriana.com/")
+    
+    for code in all_codes:
+        driver.get(post_url.format(code))
+        dom = etree.HTML(driver.page_source)
+        data = json.loads(dom.xpath("//pre/text()")[0])
+        all_locations = data.get("stores", {}).get("stores")
+        if not all_locations or len(all_locations) == 0:
+            all_codes.found_nothing()
+            continue
+        print(len(all_locations))
+        for poi in all_locations:
+            street_address = f'{poi["address1"]} {poi["address2"]}'
+            all_codes.found_location_at(poi["latitude"], poi["longitude"])
+
+            item = SgRecord(
+                locator_domain=domain,
+                page_url=start_url,
+                location_name=poi["name"],
                 street_address=street_address,
-                city=city,
-                state=state,
-                zip_postal=zip_postal,
-                country_code=country_code,
-                phone=phone,
-                hours_of_operation=hours_of_operation,
-                raw_address=raw_address,
+                city=poi["city"],
+                state=poi["stateCode"],
+                zip_postal=poi["postalCode"],
+                country_code=poi["countryCode"],
+                store_number=poi["ID"],
+                phone="",
+                location_type="",
+                latitude=poi["latitude"],
+                longitude=poi["longitude"],
+                hours_of_operation="",
             )
+            yield item
 
 
 def scrape():
-    log.info(f"start {DOMAIN} Scraper")
-    count = 0
-    with SgWriter(SgRecordDeduper(RecommendedRecordIds.StreetAddressId)) as writer:
-        results = fetch_data()
-        for rec in results:
-            writer.write_row(rec)
-            count = count + 1
-    log.info(f"No of records being processed: {count}")
-    log.info("Finished")
+    with SgWriter(
+        SgRecordDeduper(
+            SgRecordID(
+                {SgRecord.Headers.LOCATION_NAME, SgRecord.Headers.STREET_ADDRESS}
+            )
+        )
+    ) as writer:
+        for item in fetch_data():
+            writer.write_row(item)
 
 
 if __name__ == "__main__":
-    scrape()
+    with SgChrome(
+        is_headless=False, proxy_country="mx", response_successful=check_response, proxy_provider_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER
+    ) as driver:
+        scrape()
