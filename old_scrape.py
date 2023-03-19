f0b2c0f59191c4b08b18da11555bc350e64af263
extrasauceontop <msuchemba@gmail.com>
2023-03-19 14:45:29 -0500

    scrape

--- old_scrape.py
+++ old_scrape.py
@@ -4,31 +4,41 @@ from sgscrape.sgrecord import SgRecord
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 from sgscrape.sgrecord_id import SgRecordID
 from sgscrape.sgwriter import SgWriter
+from sgselenium import SgChromeWithoutSeleniumWire
 
 
 def fetch_data():
-    start_url = "https://www.sprintersports.com/api/store/by_points"
     domain = "sprintersports.com"
-    hdr = {
-        "accept": "application/json, text/plain, */*",
-        "content-type": "application/json;charset=UTF-8",
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-    }
-
-    with SgRequests() as session:
-        frm = {
-            "p1x": -49.16087237499999,
-            "p1y": 27.18198667733388,
-            "p2x": 47.694596375,
-            "p2y": 47.846820320278624,
-            "clat": 38.2430516,
-            "clon": -0.733138,
-            "market": 1,
-        }
-        data_stuff = session.post(start_url, headers=hdr, json=frm)
-        print(data_stuff.response.text)
-
-        data = data_stuff.json()
+    url = "https://www.sprintersports.com/tiendas"
+    with SgChromeWithoutSeleniumWire(is_headless=False) as driver:
+        driver.get(url)
+        data = driver.execute_async_script(
+            """
+            var done = arguments[0]
+            fetch("https://www.sprintersports.com/api/store/by_points", {
+                "headers": {
+                    "accept": "application/json, text/plain, */*",
+                    "accept-language": "en-US,en;q=0.9",
+                    "cache-control": "no-cache, no-store, must-revalidate",
+                    "content-type": "application/json;charset=UTF-8",
+                    "pragma": "no-cache",
+                    "sec-ch-ua-mobile": "?0",
+                    "sec-fetch-dest": "empty",
+                    "sec-fetch-mode": "cors",
+                    "sec-fetch-site": "same-origin",
+                    "uuid": "f3a5c125-f3b5-4d19-8d15-597aa569c4e3"
+                },
+                "referrer": "https://www.sprintersports.com/tiendas",
+                "referrerPolicy": "strict-origin-when-cross-origin",
+                "body": '{"p1x":-1.0040196528320244,"p1y":38.16211372039643,"p2x":-0.46225634716796193,"p2y":38.32389946749119,"clat":38.2430516,"clon":-0.733138,"market":1}',
+                "method": "POST",
+                "mode": "cors",
+                "credentials": "include"
+            })
+            .then(res => res.json())
+            .then(data => done(data))
+            """
+        )
         for poi in data["stores"]:
             page_url = "https://" + poi["url"]
 
