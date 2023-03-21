5a807fecb15125cf15102bfe2a15f327406334aa
extrasauceontop <msuchemba@gmail.com>
2023-03-21 14:05:59 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,71 +1,188 @@
-from sgscrape.sgrecord import SgRecord
+from sgrequests import SgRequests
+from bs4 import BeautifulSoup as bs
+import re
+from sgzip.dynamic import DynamicZipSearch, SearchableCountries, Grain_2
+from sgscrape import simple_scraper_pipeline as sp
 from sgscrape.sgrecord_deduper import SgRecordDeduper
-from sgscrape.sgrecord_id import SgRecordID
+from sgscrape.sgrecord import SgRecord
 from sgscrape.sgwriter import SgWriter
-from sgselenium import SgChromeWithoutSeleniumWire
+from sgscrape.sgrecord_id import SgRecordID
+from sglogging import SgLogSetup
+from proxyfier import ProxyProviders
+
+logger = SgLogSetup().get_logger("crawl")
 
 
-def fetch_data():
-    domain = "sprintersports.com"
-    url = "https://www.sprintersports.com/tiendas"
-    with SgChromeWithoutSeleniumWire(is_headless=False) as driver:
-        driver.get(url)
-        data = driver.execute_async_script(
-            """
-            var done = arguments[0]
-            fetch("https://www.sprintersports.com/api/store/by_points", {
-                "headers": {
-                    "accept": "application/json, text/plain, */*",
-                    "accept-language": "en-US,en;q=0.9",
-                    "cache-control": "no-cache, no-store, must-revalidate",
-                    "content-type": "application/json;charset=UTF-8",
-                    "pragma": "no-cache",
-                    "sec-ch-ua-mobile": "?0",
-                    "sec-fetch-dest": "empty",
-                    "sec-fetch-mode": "cors",
-                    "sec-fetch-site": "same-origin",
-                    "uuid": "f3a5c125-f3b5-4d19-8d15-597aa569c4e3"
-                },
-                "referrer": "https://www.sprintersports.com/tiendas",
-                "referrerPolicy": "strict-origin-when-cross-origin",
-                "body": '{"p1x":-49.16087237499999,"p1y":27.18198667733388,"p2x":47.694596375,"p2y":47.846820320278624,"clat":38.2430516,"clon":-0.733138,"market":1}',
-                "method": "POST",
-                "mode": "cors",
-                "credentials": "include"
-            })
-            .then(res => res.json())
-            .then(data => done(data))
-            """
-        )
-        for poi in data["stores"]:
-            page_url = "https://" + poi["url"]
+def get_data():
+    search = DynamicZipSearch(
+        country_codes=[SearchableCountries.BRITAIN], expected_search_radius_miles=15
+    )
 
-            item = SgRecord(
-                locator_domain=domain,
-                page_url=page_url,
-                location_name=poi["name"],
-                street_address=poi["address"],
-                city=poi["city"],
-                state=poi["province"],
-                zip_postal=poi["zip"],
-                country_code=poi["country"],
-                store_number=poi["id_point"],
-                phone=poi["phone"],
-                location_type="",
-                latitude=poi["latitude"],
-                longitude=poi["longitude"],
-                hours_of_operation="",
+    with SgRequests(proxy_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER) as session:
+        for zip_code in search:
+            search.found_nothing()
+            found = 0
+            url = (
+                "https://www.salvationarmy.org.uk/map-page?near%5Bvalue%5D="
+                + str(zip_code)
+                + "&near%5Bdistance%5D%5Bfrom%5D=40.22"
             )
+            logger.info(
+                f"Getting ready to parse zipcode {str(zip_code)}\nUrl:\n{str(url)}"
+            )
+            response_stuff = session.get(url)
+            try:
+                response = response_stuff.text
+            
+            except Exception:
+                with open("file.txt", "w", encoding="utf-8") as output:
+                    print(response.response.text, file=output)
+                raise Exception
 
-            yield item
+            soup = bs(response, "html.parser")
+
+            grids = soup.find_all(
+                "div", attrs={"class": "geolocation-location js-hide"}
+            )
+            x = 0
+            for grid in grids:
+                locator_domain = "salvationarmy.org.uk"
+
+                page_url = (
+                    "salvationarmy.org.uk" + grid.find_all("p")[-1].find("a")["href"]
+                )
+                logger.info(f"Currently parsing {str(page_url)}")
+                country_code = "UK"
+
+                location_name = grid.find(
+                    "p", attrs={"class": "field-content title"}
+                ).text.strip()
+                try:
+                    full_address = grid.find(
+                        "p", attrs={"class": "address"}
+                    ).text.strip()
+                    full_address = full_address.split("\n")
+                    full_address = [item.strip() for item in full_address]
+
+                    address = full_address[0]
+
+                    city_zipp = (
+                        full_address[1].replace("  ", " ").replace(",", "").split(" ")
+                    )
+
+                    city = ""
+                    zipp = ""
+
+                    for item in city_zipp:
+                        if re.search(r"\d", item) is None:
+                            city = city + " " + item
+                        else:
+                            zipp = zipp + " " + item
+
+                    zipp = zipp.strip()
+                    city = city.strip()
+                except Exception:
+                    logger.error("Failed to parse address, here's grid\n{str(grid)}")
+                    address = None
+                    city = None
+                    zipp = None
+                latitude = grid["data-lat"]
+                longitude = grid["data-lng"]
+
+                state = "<MISSING>"
+
+                phone_list = grid.find("a").text.strip()
+                phone = ""
+                for item in phone_list:
+                    if re.search(r"\d", item) is not None:
+                        phone = phone + item
+                    if len(phone) == 11:
+                        break
+
+                if phone == "":
+                    phone = "<MISSING>"
+
+                hours = "<MISSING>"
+
+                store_number = grid["id"]
+                try:
+                    location_type = (
+                        grid["data-icon"].split("/")[-1].split(".")[0].split("_")[0]
+                    )
+                    if location_type == "corps":
+                        location_type = "Church"
+                except Exception as e:
+                    logger.error(
+                        f"Couldn't parse loctype, here's tag \n {str(grid)}", exc_info=e
+                    )
+                # search.found_location_at(latitude, longitude)
+                found += 1
+                x = x + 1
+
+                yield {
+                    "locator_domain": locator_domain,
+                    "page_url": page_url,
+                    "location_name": location_name,
+                    "latitude": latitude,
+                    "longitude": longitude,
+                    "city": city if city else "<MISSING>",
+                    "store_number": store_number,
+                    "street_address": address if address else "<MISSING>",
+                    "state": state if state else "<MISSING>",
+                    "zip": zipp if zipp else "<MISSING>",
+                    "phone": phone,
+                    "location_type": location_type,
+                    "hours": hours,
+                    "country_code": country_code,
+                }
+            if found == 0:
+                search.found_nothing()
 
 
 def scrape():
+    field_defs = sp.SimpleScraperPipeline.field_definitions(
+        locator_domain=sp.MappingField(mapping=["locator_domain"]),
+        page_url=sp.MappingField(mapping=["page_url"]),
+        location_name=sp.MappingField(mapping=["location_name"]),
+        latitude=sp.MappingField(mapping=["latitude"]),
+        longitude=sp.MappingField(mapping=["longitude"]),
+        street_address=sp.MultiMappingField(
+            mapping=["street_address"], is_required=False
+        ),
+        city=sp.MappingField(
+            mapping=["city"],
+        ),
+        state=sp.MappingField(mapping=["state"], is_required=False),
+        zipcode=sp.MultiMappingField(mapping=["zip"], is_required=False),
+        country_code=sp.MappingField(mapping=["country_code"]),
+        phone=sp.MappingField(mapping=["phone"], is_required=False),
+        store_number=sp.MappingField(mapping=["store_number"]),
+        hours_of_operation=sp.MappingField(mapping=["hours"], is_required=False),
+        location_type=sp.MappingField(mapping=["location_type"], is_required=False),
+    )
+
     with SgWriter(
-        SgRecordDeduper(SgRecordID({SgRecord.Headers.STORE_NUMBER}))
+        deduper=SgRecordDeduper(
+            SgRecordID(
+                {
+                    SgRecord.Headers.LATITUDE,
+                    SgRecord.Headers.LONGITUDE,
+                    SgRecord.Headers.CITY,
+                    SgRecord.Headers.STATE,
+                    SgRecord.Headers.LOCATION_TYPE,
+                    SgRecord.Headers.STORE_NUMBER
+                }
+            ),
+            duplicate_streak_failure_factor=-1,
+        )
     ) as writer:
-        for item in fetch_data():
-            writer.write_row(item)
+        pipeline = sp.SimpleScraperPipeline(
+            scraper_name="Crawler",
+            data_fetcher=get_data,
+            field_definitions=field_defs,
+            record_writer=writer,
+        )
+        pipeline.run()
 
 
 if __name__ == "__main__":
