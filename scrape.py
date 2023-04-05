6323dde0bc8d8985ea7c8140467aa63aa92463d3
extrasauceontop <msuchemba@gmail.com>
2023-04-05 15:17:28 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,5 +1,6 @@
 from sgrequests import SgRequests
 from sgscrape import simple_scraper_pipeline as sp
+from sgzip.dynamic import DynamicGeoSearch, SearchableCountries, Grain_8
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 from sgscrape.sgrecord import SgRecord
 from sgscrape.sgwriter import SgWriter
@@ -7,54 +8,93 @@ from sgscrape.sgrecord_id import SgRecordID
 
 
 def get_data():
+    search = DynamicGeoSearch(
+        country_codes=[SearchableCountries.USA], granularity=Grain_8()
+    )
     session = SgRequests()
-    url = "https://api.momentfeed.com/v1/analytics/api/v2/llp/sitemap?auth_token=YNDRAXWGIEKBMEAP&country=US&multi_account=false"
-    response = session.get(url).json()
-
-    for location in response["locations"]:
-        locator_domain = "centers.consulatehealthcare.com"
-        page_url = "https://centers.consulatehealthcare.com" + location["llp_url"]
-        address = location["store_info"]["address"]
-        city = location["store_info"]["locality"]
-        state = location["store_info"]["region"]
-        zipp = location["store_info"]["postcode"]
-        location_type = "<MISSING>"
-        hours = "24/7"
-        country_code = location["store_info"]["country"]
-
-        api_url = (
-            "https://api.momentfeed.com/v1/analytics/api/llp.json?address="
-            + address.replace(" ", "+")
-            + "&locality="
-            + city.replace(" ", "+")
-            + "&multi_account=false&pageSize=30&region="
-            + state
-            + "&auth_token=YNDRAXWGIEKBMEAP"
+    page_urls = []
+    for search_lat, search_lon in search:
+        search_found = False
+        url = (
+            "https://platform.cloud.coveo.com/rest/search/v2?sitecoreItemUri=sitecore%3A%2F%2Fweb%2F%7BC020E446-D3F3-4E7C-BAF2-EEB5B6D0E0B9%7D%3Flang%3Den%26amp%3Bver%3D1&siteName=Famous%20Footwear&actionsHistory=%5B%5D&referrer=https%3A%2F%2Fwww.famousfootwear.com%2F&analytics=%7B%22clientId%22%3A%227c598e48-42e1-251a-f99f-99acdec5d752%22%2C%22documentLocation%22%3A%22https%3A%2F%2Fwww.famousfootwear.com%2Fstores%3Ficid%3Dftr_store_click_storefinder%22%2C%22documentReferrer%22%3A%22https%3A%2F%2Fwww.famousfootwear.com%2F%22%2C%22pageId%22%3A%22%22%7D&visitorId=7c598e48-42e1-251a-f99f-99acdec5d752&isGuestUser=false&aq=(%24qf(function%3A'dist(%40latitude%2C%20%40longitude%2C%20"
+            + str(search_lat)
+            + "%2C%20"
+            + str(search_lon)
+            + ")'%2C%20fieldName%3A%20'distance'))%20(%40distance%3C%3D40233.5)&cq=%40source%3D%3D20000_FamousFootwear_Catalog&searchHub=FamousStoreLocator&locale=en&maximumAge=900000&firstResult=0&numberOfResults=10&excerptLength=200&enableDidYouMean=false&sortCriteria=%40distance%20ascending&queryFunctions=%5B%5D&rankingFunctions=%5B%5D&facetOptions=%7B%7D&categoryFacets=%5B%5D&retrieveFirstSentences=true&timezone=America%2FChicago&enableQuerySyntax=false&enableDuplicateFiltering=false&enableCollaborativeRating=false&debug=false&context=%7B%22isAnonymous%22%3A%22true%22%2C%22device%22%3A%22Default%22%2C%22website%22%3A%22FamousFootwear%22%7D&allowQueriesWithoutKeywords=true"
         )
-        page_response = session.get(api_url).json()
 
-        phone = page_response[0]["store_info"]["phone"]
-        location_name = page_response[0]["store_info"]["name"]
-        latitude = page_response[0]["store_info"]["latitude"]
-        longitude = page_response[0]["store_info"]["longitude"]
-        store_number = page_response[0]["store_info"]["corporate_id"]
-
-        yield {
-            "locator_domain": locator_domain,
-            "page_url": page_url,
-            "location_name": location_name,
-            "latitude": latitude,
-            "longitude": longitude,
-            "city": city,
-            "store_number": store_number,
-            "street_address": address,
-            "state": state,
-            "zip": zipp,
-            "phone": phone,
-            "location_type": location_type,
-            "hours": hours,
-            "country_code": country_code,
+        headers = {
+            "accept": "*/*",
+            "accept-language": "en-US,en;q=0.9",
+            "authorization": "Bearer xx6b22c1da-b9c6-495b-9ae1-e3ac72612c6f",
+            "content-type": 'application/x-www-form-urlencoded; charset="UTF-8"',
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "cross-site",
         }
+        response = session.post(url, headers=headers).json()
+
+        for location in response["results"]:
+            locator_domain = "famousfootwear.com"
+            page_url = "famousfootwear.com" + location["raw"]["storedetailurl"]
+            location_name = location["title"]
+            address = location["raw"]["address1"]
+
+            if "address2" in location["raw"].keys():
+                address = address + " " + location["raw"]["address2"]
+            city = location["raw"]["city"]
+            state = location["raw"]["state"]
+
+            zipp = location["raw"]["zipcode"][:5]
+            country_code = "US"
+            store_number = location["raw"]["storeid"]
+            phone = location["raw"]["phonenumber"]
+            location_type = location["raw"]["objecttype"]
+            latitude = location["raw"]["latitude"]
+            longitude = location["raw"]["longitude"]
+            search_found = True
+            hours = (
+                "Mon "
+                + location["raw"]["mondayhours"]
+                + ", Tue "
+                + location["raw"]["tuesdayhours"]
+                + ", Wed "
+                + location["raw"]["wednesdayhours"]
+                + ", Thu "
+                + location["raw"]["thursdayhours"]
+                + ", Fri "
+                + location["raw"]["fridayhours"]
+                + ", Sat "
+                + location["raw"]["saturdayhours"]
+                + ", Sun "
+                + location["raw"]["sundayhours"]
+            )
+
+            search.found_location_at(latitude, longitude)
+            if page_url in page_urls:
+                continue
+
+            page_urls.append(page_url)
+
+            yield {
+                "locator_domain": locator_domain,
+                "page_url": page_url,
+                "location_name": location_name,
+                "latitude": latitude,
+                "longitude": longitude,
+                "city": city,
+                "store_number": store_number,
+                "street_address": address,
+                "state": state,
+                "zip": zipp,
+                "phone": phone,
+                "location_type": location_type,
+                "hours": hours,
+                "country_code": country_code,
+            }
+
+        if search_found is False:
+            search.found_nothing()
 
 
 def scrape():
