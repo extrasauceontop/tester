4cbf5053a03a42a2d3265303fff2acb5382fce63
extrasauceontop <msuchemba@gmail.com>
2023-05-02 11:02:57 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,163 +1,133 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import json
-from lxml import etree
-from sgscrape.pause_resume import CrawlStateSingleton, SerializableRequest
+import re
+import usaddress
+
+from lxml import html
 from sgscrape.sgrecord import SgRecord
-from sgscrape.sgrecord_deduper import SgRecordDeduper
-from sgscrape.sgrecord_id import SgRecordID
+from sgplaywright import SgPlaywright
 from sgscrape.sgwriter import SgWriter
-from sgselenium.sgselenium import SgChromeForCloudFlare
-from sglogging import SgLogSetup
+from sgscrape.sgrecord_deduper import SgRecordDeduper
+from sgscrape.sgrecord_id import RecommendedRecordIds
 
 
-def set_broken():
-    logger.info("setting broken")
-    recent_locs = crawl_state.get_misc_value("broken_locs")
-    for url_to_push in recent_locs:
-        logger.info("setting URL: " + url_to_push)
-        crawl_state.push_request(SerializableRequest(url=url_to_push))
+def parse_us_address(raw_address: str) -> tuple:
+    tag = {
+        "Recipient": "recipient",
+        "AddressNumber": "address1",
+        "AddressNumberPrefix": "address1",
+        "AddressNumberSuffix": "address1",
+        "StreetName": "address1",
+        "StreetNamePreDirectional": "address1",
+        "StreetNamePreModifier": "address1",
+        "StreetNamePreType": "address1",
+        "StreetNamePostDirectional": "address1",
+        "StreetNamePostModifier": "address1",
+        "StreetNamePostType": "address1",
+        "CornerOf": "address1",
+        "IntersectionSeparator": "address1",
+        "LandmarkName": "address1",
+        "USPSBoxGroupID": "address1",
+        "USPSBoxGroupType": "address1",
+        "USPSBoxID": "address1",
+        "USPSBoxType": "address1",
+        "OccupancyType": "address2",
+        "OccupancyIdentifier": "address2",
+        "SubaddressIdentifier": "address2",
+        "SubaddressType": "address2",
+        "PlaceName": "city",
+        "StateName": "state",
+        "ZipCode": "postal",
+    }
 
+    a = usaddress.tag(raw_address, tag_mapping=tag)[0]
+    adr1 = a.get("address1") or ""
+    adr2 = a.get("address2") or ""
+    street_address = f"{adr1} {adr2}".strip()
+    city = a.get("city") or ""
+    state = a.get("state") or ""
+    postal = a.get("postal") or ""
 
-def set_last_10():
-    logger.info("setting 10")
-    recent_locs = crawl_state.get_misc_value("recent_locs")
-    for url_to_push in recent_locs:
-        logger.info("setting URL: " + url_to_push)
-        crawl_state.push_request(SerializableRequest(url=url_to_push))
-
-
-def extract_json(html_string):
-    json_objects = []
-    count = 0
-
-    brace_count = 0
-    for element in html_string:
-
-        if element == "{":
-            brace_count = brace_count + 1
-            if brace_count == 1:
-                start = count
-
-        elif element == "}":
-            brace_count = brace_count - 1
-            if brace_count == 0:
-                end = count
-                try:
-                    json_objects.append(json.loads(html_string[start : end + 1]))
-                except Exception:
-                    pass
-        count = count + 1
-
-    return json_objects
-
-
-def get_urls():
-    url = "https://www.caseys.com/sitemap.xml"
-
-    driver.get(url)
-    logger.info(driver.page_source)
-    dom = etree.HTML(str(driver.page_source).replace("&lt;", "<").replace("&gt;", ">"))
-    all_urls = dom.xpath("//loc/text()")
-    for url in all_urls:
-        if "store-en-" in url.lower():
-            smurl = url
-
-    driver.get(smurl)
-    dom = etree.HTML(driver.page_source)
-
-    all_locations = dom.xpath('//loc[contains(text(), "/general-store/")]/text()')
-    x = 0
-    for url_to_push in all_locations:
-        logger.info("Pushing URL: " + url_to_push)
-        crawl_state.push_request(SerializableRequest(url=url_to_push))
-        x = x + 1
-
-    logger.info("Total URLs pushed: " + str(x))
-    crawl_state.set_misc_value("got_urls", True)
+    return street_address, city, state, postal
 
 
 def fetch_data():
-    most_recent_locs = []
-    broken_locs = []
-    class_name = "h-100"
-    for page_url_thing in crawl_state.request_stack_iter():
-        page_url = page_url_thing.url
-        most_recent_locs.append(page_url)
-        if len(most_recent_locs) > 10:
-            most_recent_locs = most_recent_locs[-10:]
-            crawl_state.set_misc_value("got_last_ten", True)
-            crawl_state.set_misc_value("recent_locs", most_recent_locs)
-        logger.info(page_url)
-        driver.get(page_url)
-        try:
-            WebDriverWait(driver, 20).until(
-                EC.presence_of_element_located((By.CLASS_NAME, class_name))
-            )
-        except Exception:
-            logger.info("Skipping broken URL: " + page_url)
-            crawl_state.set_misc_value("got_broken", True)
-            broken_locs.append(page_url)
-            crawl_state.set_misc_value("broken_locs", most_recent_locs)
-            continue
-        loc_dom = etree.HTML(driver.page_source)
-        response = driver.page_source
-        poi = extract_json(response.split("ld+json")[1])[0]
+    api = "https://www.serenaandlily.com/stores.html"
+    with SgPlaywright(headless=False).chrome() as fox:
+        fox.goto(api)
+        source = fox.content()
+        tree = html.fromstring(source)
 
-        longitude = loc_dom.xpath("//@initlongitudevalue")[0]
-        latitude = loc_dom.xpath("//@initlatitudevalue")[0]
+    divs = tree.xpath(
+        "//div[contains(@class, '-region col-sm-6') and .//*[text()='BOOK US']]"
+    )
+    for d in divs:
+        slug = "".join(d.xpath(".//a[contains(@href, '/stores')]/@href"))
+        if slug:
+            page_url = f"https://www.serenaandlily.com{slug}"
+        else:
+            page_url = api
+
+        if d.xpath(".//p[contains(text(), 'Opening')]"):
+            continue
+
+        names = d.xpath(
+            ".//div[count(./p)<=2 and not(.//a[contains(text(), 'VISIT')])]/p//text()"
+        )
+        if len(names) > 2:
+            location_name = names[0]
+        else:
+            location_name = names.pop()
+
+        line = d.xpath(".//div[count(./p)>1]//text()")
+        line = list(filter(None, [l.replace("\ufeff", "").strip() for l in line]))
+
+        cnt = 0
+        for li in line:
+            if "email" in li:
+                break
+            cnt += 1
+
+        line = line[:cnt]
+        phone = line.pop()
+
+        raw = []
+        for li in line:
+            if "Now" in li or (li[0].isalpha() and li[-1].isalpha()):
+                continue
+            raw.append(li)
+            if re.findall(r"[A-Z]{2} \d{5}", li):
+                break
+
+        raw_address = ", ".join(raw)
+        street_address, city, state, postal = parse_us_address(raw_address)
+
+        hours_of_operation = ";".join(line).split(f"{postal};")[-1]
+        text = "".join(d.xpath(".//a[contains(@href, 'google')]/@href"))
         try:
-            city = poi["address"]["addressLocality"]
-        except Exception:
-            city = "<MISSING>"
-        store_number = page_url.split("/")[-1]
-        item = SgRecord(
-            locator_domain=domain,
+            latitude, longitude = text.split("/@")[1].split(",")[:2]
+        except IndexError:
+            latitude, longitude = SgRecord.MISSING, SgRecord.MISSING
+
+        row = SgRecord(
             page_url=page_url,
-            location_name=poi["name"],
-            street_address=poi["address"]["streetAddress"],
+            location_name=location_name,
+            street_address=street_address,
             city=city,
-            state=poi["address"]["addressregion"],
-            zip_postal=poi["address"]["postalCode"],
-            country_code=poi["address"]["addressCountry"],
-            store_number=store_number,
-            phone=poi["telephone"],
-            location_type=poi["@type"],
+            state=state,
+            zip_postal=postal,
+            country_code="US",
+            phone=phone,
             latitude=latitude,
             longitude=longitude,
-            hours_of_operation=" ".join(poi["openingHours"]),
+            locator_domain=locator_domain,
+            raw_address=raw_address,
+            hours_of_operation=hours_of_operation,
         )
-        logger.info("Yielding item")
-        logger.info(poi)
-        yield item
 
-
-def scrape():
-    with SgWriter(SgRecordDeduper(SgRecordID({SgRecord.Headers.PAGE_URL}))) as writer:
-        for item in fetch_data():
-            writer.write_row(item)
-
-        if crawl_state.get_misc_value("got_broken"):
-            set_broken()
-            for item in fetch_data():
-                writer.write_row(item)
+        sgw.write_row(row)
 
 
 if __name__ == "__main__":
-    with SgChromeForCloudFlare(
-        proxy_country="us",
-        is_headless=False,
-        proxy_provider_escalation_order=["http://groups-RESIDENTIAL,country-us:{}@proxy.apify.com:8000/"]
-    ) as driver:
-        domain = "caseys.com"
-        logger = SgLogSetup().get_logger(domain)
-        crawl_state = CrawlStateSingleton.get_instance()
+    locator_domain = "https://www.serenaandlily.com/"
 
-        if not crawl_state.get_misc_value("got_urls"):
-            get_urls()
-
-        if crawl_state.get_misc_value("got_last_ten"):
-            set_last_10()
-
-        scrape()
+    with SgWriter(SgRecordDeduper(RecommendedRecordIds.PhoneNumberId)) as sgw:
+        fetch_data()
