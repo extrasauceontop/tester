afbd49c2b8b82fff9a706883f87658be8ecf1bb1
extrasauceontop <msuchemba@gmail.com>
2023-05-02 15:19:54 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,109 +1,164 @@
-from sgplaywright import SgPlaywright
-from sgscrape.sgrecord_deduper import SgRecordDeduper
+from sgselenium import SgChromeWithoutSeleniumWire
+from lxml import html
 from sgscrape.sgrecord import SgRecord
 from sgscrape.sgwriter import SgWriter
-from sgscrape.sgrecord_id import SgRecordID
-from sgscrape import simple_scraper_pipeline as sp
-from bs4 import BeautifulSoup as bs
+from sgscrape.sgrecord_deduper import SgRecordDeduper
+from sgscrape.sgrecord_id import RecommendedRecordIds
+from sglogging import sglog
+from sgscrape.sgpostal import parse_address, International_Parser
+from sgscrape.pause_resume import CrawlStateSingleton, SerializableRequest
+import re
+
+crawl_state = CrawlStateSingleton.get_instance()
 
 
-def get_data():
-    api = "https://www.serenaandlily.com/stores.html"
-    with SgPlaywright(headless=False).chrome() as fox:
-        fox.goto(api)
-        response = fox.content()
-        soup = bs(response, "html.parser")
+def check_response(dresponse):  # noqa
+    response = driver.page_source
+    if "Just a moment..." in response:
+        return False
 
-    grids = [div for div in soup.find_all("div", attrs={"class": "ModularColumnStyled__ModularContentContainer-sc-14jgp1c-2"}) if "book us" in div.text.strip().lower()]
-    
-    for grid in grids:
-        search_index = -6
-        locator_domain = "serenaandlily.com"
-        page_url = "https://www.serenaandlily.com/stores"
-        location_name = grid.find("h3").text.strip()
-        if "now open" in location_name.lower():
-            location_name = grid.find_all("h3")[1].text.strip()
-        try:
-            lat_lon_parts = grid.find("a", attrs={"title": "visit us"})["href"]
-        except Exception:
-            lat_lon_parts = grid.find("a", attrs={"title": "book us"})["href"]
-        latitude = lat_lon_parts.split("@")[1].split(",")[0]
-        longitude = lat_lon_parts.split("@")[1].split(",")[1]
-        if "monday" in grid.find_all("p")[search_index].text.strip().lower():
-            search_index = -7
-        city = grid.find_all("p")[search_index].text.strip().split(", ")[0]
-        address = grid.find("p").text.strip()
-        state = grid.find_all("p")[search_index].text.strip().split(", ")[1].split(" ")[0]
-        zipp = grid.find_all("p")[search_index].text.strip().split(", ")[1].split(" ")[1]
-        store_number = "<MISSING>"
-        phone = grid.find("a")["href"].split(":")[-1]
-        location_type = "<MISSING>"
-        if search_index == -6:
-            hours = grid.find_all("p")[-5].text.strip() + ", " + grid.find_all("p")[-4].text.strip()
-        else:
-            hours = grid.find_all("p")[-6].text.strip() + ", " + grid.find_all("p")[-5].text.strip() + ", " + grid.find_all("p")[-4].text.strip()
-        country_code = "US"
-        yield {
-            "locator_domain": locator_domain,
-            "page_url": page_url,
-            "location_name": location_name,
-            "latitude": latitude,
-            "longitude": longitude,
-            "city": city,
-            "street_address": address,
-            "state": state,
-            "zip": zipp,
-            "store_number": store_number,
-            "phone": phone,
-            "location_type": location_type,
-            "hours": hours,
-            "country_code": country_code,
-        }
+    if "ShopLocator/LocationDetail" in driver.current_url:
+        tree = html.fromstring(response)
+        location_name = "".join(
+            tree.xpath("//div[@id='shoplocator']/header/text()")
+        ).strip()
+        if location_name == "":
+            return False
+
+    return True
 
 
-def scrape():
-    field_defs = sp.SimpleScraperPipeline.field_definitions(
-        locator_domain=sp.MappingField(mapping=["locator_domain"]),
-        page_url=sp.MappingField(mapping=["page_url"]),
-        location_name=sp.MappingField(mapping=["location_name"]),
-        latitude=sp.MappingField(mapping=["latitude"]),
-        longitude=sp.MappingField(mapping=["longitude"]),
-        street_address=sp.MultiMappingField(
-            mapping=["street_address"], is_required=False
-        ),
-        city=sp.MappingField(
-            mapping=["city"],
-        ),
-        state=sp.MappingField(mapping=["state"], is_required=False),
-        zipcode=sp.MultiMappingField(mapping=["zip"], is_required=False),
-        country_code=sp.MappingField(mapping=["country_code"]),
-        phone=sp.MappingField(mapping=["phone"], is_required=False),
-        store_number=sp.MappingField(mapping=["store_number"]),
-        hours_of_operation=sp.MappingField(mapping=["hours"], is_required=False),
-        location_type=sp.MappingField(mapping=["location_type"], is_required=False),
+def get_international(line):
+    adr = parse_address(International_Parser(), line)
+    adr1 = adr.street_address_1 or ""
+    adr2 = adr.street_address_2 or ""
+    street = f"{adr1} {adr2}".strip()
+    city = adr.city or SgRecord.MISSING
+    return street, city
+
+
+def get_params():
+    driver.get("https://www.boylesports.com/ShopLocator")
+    r = driver.page_source
+    tree = html.fromstring(r)
+    source = "".join(tree.xpath("//div[@id='div-shop-locations']/text()")).strip()
+    log.info(len(eval(source)))
+    for location in eval(source):
+        url_to_push = (
+            "https://www.boylesports.com/ShopLocator/LocationDetail/?=="
+            + str(location["ShopCode"])
+            + "?=="
+            + str(location["Latitude"])
+            + "?=="
+            + str(location["Longitude"])
+        )
+        crawl_state.push_request(SerializableRequest(url=url_to_push))
+    crawl_state.set_misc_value("got_urls", True)
+
+
+def get_data(p):
+    store_number = p.url.split("?==")[1]
+    latitude = p.url.split("?==")[2]
+    longitude = p.url.split("?==")[3]
+    page_url = f"https://www.boylesports.com/ShopLocator/LocationDetail/{store_number}"
+    driver.get(page_url)
+    r = driver.page_source
+    log.info(page_url)
+    tree = html.fromstring(r)
+    location_name = "".join(
+        tree.xpath("//div[@id='shoplocator']/header/text()")
+    ).strip()
+    raw_address = ", ".join(tree.xpath("//p[@class='address']/text()"))
+    postal = raw_address.split(", ")[-1]
+    if re.search(r"\d", postal) is False:
+        postal = "<MISSING>"
+
+    else:
+        raw_address = ", ".join(tree.xpath("//p[@class='address']/text()")[:-1])
+    street_address, city = get_international(raw_address)
+    country_code = "GB"
+    phone = "".join(tree.xpath("//p[@class='phonenumber']/text()")).strip()
+    if ":" in phone:
+        phone = phone.split(":")[-1].strip()
+    hours_of_operation = ";".join(
+        tree.xpath("//strong[contains(text(), 'Hours')]/following-sibling::p/text()")
+    ).strip()
+
+    if bool(re.search(r"\d", postal)) is False:
+        postal = "<MISSING>"
+
+    row = SgRecord(
+        page_url=page_url,
+        location_name=location_name,
+        street_address=street_address,
+        city=city,
+        zip_postal=postal,
+        country_code=country_code,
+        store_number=store_number,
+        latitude=latitude,
+        longitude=longitude,
+        phone=phone,
+        locator_domain=locator_domain,
+        raw_address=raw_address,
+        hours_of_operation=hours_of_operation,
     )
 
-    with SgWriter(
-        deduper=SgRecordDeduper(
-            SgRecordID(
-                {
-                    SgRecord.Headers.LATITUDE,
-                    SgRecord.Headers.LONGITUDE,
-                    SgRecord.Headers.PAGE_URL,
-                    SgRecord.Headers.LOCATION_NAME,
-                }
-            ),
-            duplicate_streak_failure_factor=100,
-        )
-    ) as writer:
-        pipeline = sp.SimpleScraperPipeline(
-            scraper_name="Crawler",
-            data_fetcher=get_data,
-            field_definitions=field_defs,
-            record_writer=writer,
-        )
-        pipeline.run()
+    sgw.write_row(row)
+
+
+def fetch_data():
+    if not crawl_state.get_misc_value("got_urls"):
+        get_params()
+
+    for page_url_obj in crawl_state.request_stack_iter():
+        try:
+            get_data(page_url_obj)
+
+        except Exception:
+            crawl_state.push_request(SerializableRequest(url=page_url_obj.url))
 
 
 if __name__ == "__main__":
-    scrape()
+    if not crawl_state.get_misc_value("set_count"):
+        crawl_state.set_misc_value("count", 0)
+        crawl_state.set_misc_value("set_count", True)
+
+    count = crawl_state.get_misc_value("count")
+    while count < 3:
+        count = count + 1
+        crawl_state.set_misc_value("count", count)
+        x = 0
+        while True:
+            x = x + 1
+            if x == 10:
+                raise Exception
+
+            try:
+                with SgChromeWithoutSeleniumWire(
+                    is_headless=False,
+                    proxy_country="gb",
+                    page_meets_expectations=check_response,
+                ) as driver:
+                    locator_domain = "https://www.boylesports.com/"
+                    log = sglog.SgLogSetup().get_logger(logger_name="boylesports.com")
+                    headers = {
+                        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:103.0) Gecko/20100101 Firefox/103.0",
+                        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+                        "Connection": "keep-alive",
+                        "Upgrade-Insecure-Requests": "1",
+                        "Sec-Fetch-Dest": "document",
+                        "Sec-Fetch-Mode": "navigate",
+                        "Sec-Fetch-Site": "cross-site",
+                    }
+                    with SgWriter(
+                        SgRecordDeduper(RecommendedRecordIds.PageUrlId)
+                    ) as sgw:
+                        fetch_data()
+
+                break
+
+            except Exception as e:
+                log.info(e)
+                continue
+
+        crawl_state.set_misc_value("got_urls", False)
