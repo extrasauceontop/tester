ac809afcd1b93d5ec4cc3ecb80bd15fcd451310c
extrasauceontop <msuchemba@gmail.com>
2023-05-23 12:36:22 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -17,5 +17,142 @@ with SgFirefox(
     driver.get(url)
     response = driver.page_source
 
+if "Just a moment..." not in response:
+    print("SUCCESS")
+    raise Exception
 
-print(response)
+with SgChrome(
+    is_headless=False,
+    proxy_provider_escalation_order=["http://groups-RESIDENTIAL,country-nz:{}@proxy.apify.com:8000/"],
+    response_successful=check_response
+) as driver:
+    driver.get(url)
+    response = driver.page_source
+
+if "Just a moment..." not in response:
+    print("SUCCESS")
+    raise Exception
+
+with SgChromeWithoutSeleniumWire(
+    is_headless=False,
+    proxy_provider_escalation_order=["http://groups-RESIDENTIAL,country-nz:{}@proxy.apify.com:8000/"],
+    response_meets_expectations=check_response
+) as driver:
+    driver.get(url)
+    response = driver.page_source
+
+if "Just a moment..." not in response:
+    print("SUCCESS")
+    raise Exception
+
+with SgChromeForCloudFlare(
+    is_headless=False,
+    proxy_provider_escalation_order=["http://groups-RESIDENTIAL,country-nz:{}@proxy.apify.com:8000/"],
+    response_successful=check_response
+) as driver:
+    driver.get(url)
+    response = driver.page_source
+
+if "Just a moment..." not in response:
+    print("SUCCESS")
+    raise Exception
+
+with SgFirefox(
+    is_headless=True,
+    proxy_provider_escalation_order=["http://groups-RESIDENTIAL,country-nz:{}@proxy.apify.com:8000/"],
+    response_successful=check_response
+) as driver:
+    driver.get(url)
+    response = driver.page_source
+
+if "Just a moment..." not in response:
+    print("SUCCESS")
+    raise Exception
+
+with SgChrome(
+    is_headless=True,
+    proxy_provider_escalation_order=["http://groups-RESIDENTIAL,country-nz:{}@proxy.apify.com:8000/"],
+    response_successful=check_response
+) as driver:
+    driver.get(url)
+    response = driver.page_source
+
+if "Just a moment..." not in response:
+    print("SUCCESS")
+    raise Exception
+
+with SgChromeWithoutSeleniumWire(
+    is_headless=True,
+    proxy_provider_escalation_order=["http://groups-RESIDENTIAL,country-nz:{}@proxy.apify.com:8000/"],
+    response_meets_expectations=check_response
+) as driver:
+    driver.get(url)
+    response = driver.page_source
+
+if "Just a moment..." not in response:
+    print("SUCCESS")
+    raise Exception
+
+with SgChromeForCloudFlare(
+    is_headless=True,
+    proxy_provider_escalation_order=["http://groups-RESIDENTIAL,country-nz:{}@proxy.apify.com:8000/"],
+    response_successful=check_response
+) as driver:
+    driver.get(url)
+    response = driver.page_source
+
+if "Just a moment..." not in response:
+    print("SUCCESS")
+    raise Exception
+
+with SgFirefox(
+    is_headless=False,
+    proxy_provider_escalation_order=["http://groups-RESIDENTIAL,country-nz:{}@proxy.apify.com:8000/"],
+    response_successful=check_response,
+    block_third_parties=False
+) as driver:
+    driver.get(url)
+    response = driver.page_source
+
+if "Just a moment..." not in response:
+    print("SUCCESS")
+    raise Exception
+
+with SgChrome(
+    is_headless=False,
+    proxy_provider_escalation_order=["http://groups-RESIDENTIAL,country-nz:{}@proxy.apify.com:8000/"],
+    response_successful=check_response,
+    block_third_parties=False
+) as driver:
+    driver.get(url)
+    response = driver.page_source
+
+if "Just a moment..." not in response:
+    print("SUCCESS")
+    raise Exception
+
+with SgChromeWithoutSeleniumWire(
+    is_headless=False,
+    proxy_provider_escalation_order=["http://groups-RESIDENTIAL,country-nz:{}@proxy.apify.com:8000/"],
+    response_meets_expectations=check_response,
+    block_third_parties=False
+) as driver:
+    driver.get(url)
+    response = driver.page_source
+
+if "Just a moment..." not in response:
+    print("SUCCESS")
+    raise Exception
+
+with SgChromeForCloudFlare(
+    is_headless=False,
+    proxy_provider_escalation_order=["http://groups-RESIDENTIAL,country-nz:{}@proxy.apify.com:8000/"],
+    response_successful=check_response,
+    block_third_parties=False
+) as driver:
+    driver.get(url)
+    response = driver.page_source
+
+if "Just a moment..." not in response:
+    print("SUCCESS")
+    raise Exception
