b9d078794c5b868abd0c5d73a64fa889d68798f0
extrasauceontop <msuchemba@gmail.com>
2023-07-08 16:38:38 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,133 +1,218 @@
-import json
-import time
-from lxml import html
-from sglogging import SgLogSetup
-from sgplaywright import SgPlaywright
+import re
+from io import BytesIO
+from bs4 import BeautifulSoup
+from sgrequests import SgRequests
+from sgpostal.sgpostal import USA_Best_Parser, parse_address
 from sgscrape.sgrecord import SgRecord
 from sgscrape.sgwriter import SgWriter
+from sgscrape.sgrecord_id import SgRecordID
 from sgscrape.sgrecord_deduper import SgRecordDeduper
-from sgscrape.sgrecord_id import RecommendedRecordIds
-from sgscrape.sgpostal import parse_address, International_Parser
-import os
+import subprocess
+import sys
 
-os.system('DEFAULT_PROXY_ESCALATION_ORDER="http://groups-RESIDENTIAL,country-nz:{}@proxy.apify.com:8000/"')
-
-
-def parse_international(raw_address: str) -> tuple:
-    adr = parse_address(International_Parser(), raw_address, country="NZ")
-    adr1 = adr.street_address_1 or ""
-    adr2 = adr.street_address_2 or ""
-    street_address = f"{adr1} {adr2}".strip()
-    city = adr.city or ""
-    state = adr.state or ""
-    postal = adr.postcode or ""
-
-    return street_address, city, state, postal
+subprocess.check_call([sys.executable, "-m", "pip", "install", "install-jdk"])
+import jdk
+import tabula as tb  # noqa
+jdk.install(20, jre=True)
+DOMAIN = "anderinger.com"
 
 
-def get_urls():
-    def check_response(dresponse):  # noqa
-        source = driver.content()
-        tree = html.fromstring(source)
-        slugs = tree.xpath("//div[@id='content']//p/a/@href")
-        if len(slugs) == 0:
-            return False
-        return True
-
-    api = "https://www.newworld.co.nz/store-finder"
-    with SgPlaywright(
-        proxy_country="nz", headless=False, response_successful=check_response
-    ).firefox() as driver:
-        driver.goto(api)
-        time.sleep(30)
-        source = driver.content()
-        tree = html.fromstring(source)
-        return tree.xpath("//div[@id='content']//p/a/@href")
-
-
-def get_data():
-    def check_response(dresponse):  # noqa
-        source = driver.content()
-        tree = html.fromstring(source)
-        slugs = tree.xpath("//div[@id='content']//p/a/@href")
-        if len(slugs) == 0:
-            return False
-        return True
-
-    slugs = get_urls()
-    log.info(f"{len(slugs)} URLs to crawl..")
-
-    for slug in slugs:
-        x = 0
-        while True:
-            x = x + 1
-            if x == 10:
-                raise Exception
-            try:
-                page_url = f"https://www.newworld.co.nz{slug}"
-                with SgPlaywright(
-                    proxy_country="nz",
-                    headless=False,
-                    response_successful=check_response,
-                ).firefox() as driver:
-                    driver.goto(page_url)
-                    time.sleep(30)
-                    source = driver.content()
-                    break
-            except Exception:
-                continue
-        tree = html.fromstring(source)
-        log.info(f"{page_url}: success..")
-
-        location_name = "".join(tree.xpath("//h2/text()")).strip()
-        raw_address = "".join(
-            tree.xpath("//p[contains(@class, 'info-address')]/text()")
-        ).strip()
-        street_address, city, state, postal = parse_international(raw_address)
-        phone = "".join(
-            tree.xpath("//a[contains(@class, 'info-phone')]//text()")
-        ).strip()
-
-        try:
-            text = "".join(
-                tree.xpath("//div[@data-module='storedetails']/@data-module-options")
+def write_output(data):
+    with SgWriter(
+        SgRecordDeduper(
+            SgRecordID(
+                {
+                    SgRecord.Headers.STATE,
+                    SgRecord.Headers.LOCATION_NAME,
+                    SgRecord.Headers.PHONE,
+                }
             )
-            j = json.loads(text)
-            latitude = j.get("latitude")
-            longitude = j.get("longitude")
-        except:
-            latitude = longitude = SgRecord.MISSING
+        )
+    ) as writer:
+        for row in data:
+            writer.write_row(row)
 
-        _tmp = []
-        hours = tree.xpath("//table[contains(@class, 'opening-hours')]//tr")
-        for h in hours:
-            day = "".join(h.xpath("./td[1]/text()")).strip()
-            inter = "".join(h.xpath("./td[2]/text()")).strip()
-            _tmp.append(f"{day}: {inter}")
-        hours_of_operation = ";".join(_tmp)
 
-        row = SgRecord(
-            page_url=page_url,
-            location_name=location_name,
-            street_address=street_address,
-            city=city,
-            state=state,
-            zip_postal=postal,
-            country_code="NZ",
-            latitude=latitude,
-            longitude=longitude,
-            phone=phone,
-            raw_address=raw_address,
-            locator_domain=locator_domain,
-            hours_of_operation=hours_of_operation,
+def read_pdf(pdf_url):
+    table_column_boundaries = [150, 300, 430, 580]
+    area = [0.0, 0.0, 800.0, 600.0]
+    with SgRequests() as session:
+        response = session.get(pdf_url, headers={"User-Agent": "PostmanRuntime/7.19.0"})
+        file = BytesIO(response.content)
+
+        dataframes = tb.read_pdf(
+            file,
+            pages="all",
+            area=area,
+            lattice=True,
+            columns=table_column_boundaries,
         )
 
-        sgw.write_row(row)
+    data = []
+    df = dataframes[0]
+    for column in range(0, len(df.columns)):
+        if not isinstance(df.iloc[0, column], str):
+            data.append(
+                df.columns[column]
+            )  # data parsed out as column name in pd dataframe
+        else:
+            data.append(df.iloc[0, column])
+
+    unformatted = "\n".join(data)
+    return re.sub("\r", "\n", unformatted)
+
+
+def group_by_state(data):
+    lines = data.split("\n")
+
+    buckets = {}
+    current = None
+    for line in lines:
+        if line.isupper() and "PO BOX" not in line:
+            buckets[line] = []
+            current = buckets[line]
+            continue
+
+        current.append(line)
+
+    return buckets
+
+
+def group_by_city(states):
+    cities = {}
+    current_state = None
+    current_city = None
+    current_city_data = None
+
+    for state, lines in states.items():
+        current_state = state
+        for idx, line in enumerate(lines):
+            if (
+                re.search(r"–|warehous*|facility", line, flags=re.IGNORECASE)
+                or idx == 0
+            ):
+                if current_city_data:
+                    cities[current_city] = current_city_data
+
+                current_city = (
+                    f"{current_state.capitalize()} {line}"
+                    if re.search(r"warehous*", line, flags=re.IGNORECASE)
+                    else line
+                )
+                current_city = re.sub(r"\:|\;", "", current_city)
+
+                current_city_data = []
+                current_city_data.append(current_state)
+                current_city_data.append(line)
+
+            else:
+                current_city_data.append(line)
+
+            if idx == len(lines) - 1:
+                cities[current_city] = current_city_data
+
+    return cities
+
+
+def get_phone(data):
+    for line in data:
+        if re.search(r"tel\s*:\s*", line, re.IGNORECASE):
+            return re.sub(r"tel\s*:\s*|\s*\|\s*.*", "", line, flags=re.IGNORECASE)
+
+
+def get_store_number(name):
+    if "–" in name:
+        parsed = name.split(" – ")
+        return parsed.pop()
+
+    return MISSING
+
+
+def get_location_type(data):
+    if re.search("headquarter", data[0], flags=re.IGNORECASE):
+        return "CORPORATE HEADQUARTERS"
+
+    return MISSING
+
+
+def get_address(data):
+    address = ",".join(data[2:4])
+
+    if "PO BOX" in address:
+        address = ",".join(data[4:6])
+
+    if re.search(r"(tel|fax)\s*:\s*", address, flags=re.IGNORECASE):
+        address = ""
+
+    return address
+
+
+MISSING = "<MISSING>"
+
+
+def extract(name, data, pdf_url):
+    address = get_address(data)
+
+    parsed_address = parse_address(USA_Best_Parser(), address)
+
+    locator_domain = "anderinger.com"
+    page_url = pdf_url
+    location_name = name
+    street_address = parsed_address.street_address_1
+    if parsed_address.street_address_2:
+        street_address += f", {parsed_address.street_address_2}"
+    city = parsed_address.city
+    state = parsed_address.state
+    postal = parsed_address.postcode
+    country_code = parsed_address.country
+    store_number = get_store_number(name)
+    phone = get_phone(data)
+    location_type = get_location_type(data)
+    latitude = MISSING
+    longitude = MISSING
+    hours_of_operation = MISSING
+
+    return SgRecord(
+        locator_domain=locator_domain,
+        page_url=page_url,
+        location_name=location_name,
+        street_address=street_address,
+        city=city,
+        state=state,
+        zip_postal=postal,
+        country_code=country_code,
+        store_number=store_number,
+        phone=phone,
+        location_type=location_type,
+        latitude=latitude,
+        longitude=longitude,
+        hours_of_operation=hours_of_operation,
+    )
+
+
+def fetch_data():
+
+    base_link = "https://www.anderinger.com/about-deringer/locations/"
+
+    with SgRequests() as session:
+        req = session.get(base_link, headers={"User-Agent": "PostmanRuntime/7.19.0"})
+        base = BeautifulSoup(req.text, "lxml")
+        pdf_url = base.find(class_="entry-content").a["href"]
+
+    data = read_pdf(pdf_url)
+    states = group_by_state(data)
+    locations = group_by_city(states)
+
+    for name, data in locations.items():
+        poi = extract(name, data, pdf_url)
+        if poi:
+            yield poi
+
+
+def scrape():
+    data = fetch_data()
+    write_output(data)
 
 
 if __name__ == "__main__":
-    locator_domain = "https://www.newworld.co.nz/"
-    log = SgLogSetup().get_logger(logger_name="newworld.co.nz")
-    with SgWriter(SgRecordDeduper(RecommendedRecordIds.PageUrlId)) as sgw:
-        get_data()
-
+    scrape()
